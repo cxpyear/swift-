827cb9153d7b7549f193e41831e0bfc5deb74127
GBTouchG3 <2270991095@qq.com>
2015-08-07 13:48:20 +0800

    Initial Commit

---
+++ 原型设计模式/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  原型设计模式
+//
+//  Created by GBTouchG3 on 15/8/7.
+//  Copyright (c) 2015年 GBTouchG3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
